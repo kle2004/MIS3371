95fd54da32e14823bd29b24c671c78f88749a876
Miniimei <kaitlynmimi02@gmail.com>
2025-12-11 18:06:59 -0600

    Create homework1.js

---
+++ homework1.js
@@ -0,0 +1,13 @@
+   /*
+    Program name: homework1.js
+    Author: Hoan My Le
+    Date created: 2025-09-10
+    Date last edited: 2025-09-26
+    Version: 1.0
+    Description: MIS 3371 Homework 1... learning JavaScript and creating a form
+*/
+
+function goToPage() 
+{
+  window.location.href = 'https://bauer.uh.edu/';
+}
