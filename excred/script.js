9297df293061848d38947212d8a8c62614b15ac4
Miniimei <kaitlynmimi02@gmail.com>
2025-12-18 14:13:32 -0600

    Update script.js

--- excred/script.js
+++ excred/script.js
@@ -472,3 +472,13 @@ function passwordentry()
   }
   document.getElementById("pwmessage5").innerHTML = passwordoutput;
   }
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
